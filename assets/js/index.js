997521ec20ccccd51cb7af4e4bfad07b9f574f50
Authentic10 <ahmedtchagnaou@gmail.com>
2021-05-19 07:03:28 +0200

    update submodule files

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,3 +17,33 @@ function initBurger() {
 }
 
 initBurger();
+
+/* Change mode script */
+
+/*function changeMode() {
+  var switcher = document.getElementById('mode');
+  var element = document.body;
+  var links = document.getElementById('navigation');
+
+
+  if(!switcher.checked){
+    //alert('checked');
+    element.classList.toggle("dark-mode");
+    links.classList.toggle("dark-mode");
+    //switcher.checked = true;
+    localStorage.setItem('darkMode','true');
+  } else {
+    element.classList.toggle("dark-mode");
+    links.classList.toggle("dark-mode");
+    localStorage.removeItem('darkMode');
+  }
+
+  if(localStorage.getItem('darkMode')){
+    //alert('Dark');
+    //switcher.checked = true;
+    //element.classList.toggle("dark-mode");
+  }
+
+  //const saved = localStorage.getItem("") || "auto";
+  //element.classList.toggle("dark-mode");
+}*/
